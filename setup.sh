1726f0c3446fa207a00f230bb8a653e120255877
EC2 Default User <ec2-user@ip-10-0-15-114.ap-southeast-2.compute.internal>
2024-01-24 02:55:52 +0000

    including logic to install python

--- setup.sh
+++ setup.sh
@@ -8,10 +8,54 @@ cd RTC
 sh build_docker_image_otf.sh
 cd ..
 
-# make virtual env for downloads
-source ~/.bashrc
-pip install virtualenv
-python -m venv rtc_otf_env
-source rtc_otf_env/bin/activate
-pip install -r requirements.txt
+INSTALL_PYTHON=true
 
+# Process command-line arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -p|--install-python)
+            INSTALL_PYTHON=true
+            ;;
+        -np|--no-install-python)
+            INSTALL_PYTHON=false
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+# Python installation, needed on aws image for management
+if [ "$INSTALL_PYTHON" = true ]; then
+    echo "Installing Python3.9..."
+    # Your Python installation commands here
+    sudo yum install gcc openssl-devel bzip2-devel libffi-devel zlib-devel
+    wget https://www.python.org/ftp/python/3.9.6/Python-3.9.6.tgz
+    tar -xvf Python-3.9.6.tgz
+    cd Python-3.9.6
+    ./configure --enable-optimizations
+    sudo make
+    sudo make altinstall
+    python3.9 --version
+    cd ..
+    sudo rm -r Python-3.9.6
+    sudo rm -r Python-3.9.6.tgz
+
+    # make virtual env for downloads
+    source ~/.bashrc
+    python3.9 -m pip install virtualenv
+    python3.9 -m venv rtc_otf_env
+    source rtc_otf_env/bin/activate
+    pip install -r requirements.txt
+fi
+
+if [ "$INSTALL_PYTHON" = false ]; then
+    # make virtual env for downloads
+    source ~/.bashrc
+    python -m pip install virtualenv
+    python -m venv rtc_otf_env
+    source rtc_otf_env/bin/activate
+    pip install -r requirements.txt
+fi
